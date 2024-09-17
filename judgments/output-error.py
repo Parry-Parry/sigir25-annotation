5d583ded95ff6c81acedf925c323d2fa675247e4
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-09-17 10:03:57 +0100

    adding error checker

---
+++ judgments/output-error.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+from glob import glob
+import os
+import json
+import ir_datasets
+
+dataset = ir_datasets.load('msmarco-passage/trec-dl-2019/judged')
+qrels = {}
+for qrel in dataset.qrels_iter():
+    if qrel.query_id not in qrels:
+        qrels[qrel.query_id] = {}
+    qrels[qrel.query_id][qrel.doc_id] = qrel.relevance
+
+dataset = ir_datasets.load('msmarco-passage/trec-dl-2020/judged')
+for qrel in dataset.qrels_iter():
+    if qrel.query_id not in qrels:
+        qrels[qrel.query_id] = {}
+    qrels[qrel.query_id][qrel.doc_id] = qrel.relevance
+
+LOOKUP = {}
+
+def to_qrels(file_name):
+
+    print(file_name)
+    with open(f'main/doccano/{file_name}.jsonl', 'r') as src, open(f'main/qrels/{file_name}-qrels.txt', 'w') as target:
+        LOOKUP[file_name] = {
+            'missing' : [], 
+            'duplicates' : []
+        }
+        for i, l in src:
+            l = json.loads(l)
+            if len(l['label']) < 1:
+                LOOKUP[file_name]['missing'].append(i)
+                continue
+            if len(l['label']) > 1:
+                LOOKUP[file_name]['duplicates'].append(i)
+                #raise ValueError('Missing judgment')
+            
+if __name__ == '__main__':
+    for f in ['harry-scells', 'froebe', 'guglielmo-faggioli', 'andrew-parry', 'ferdinand-schlatt', 'saber-zerhoudi', 'sean-macavaney', 'eugene-yang']: #glob('pilot-round-01/doccano/*.jsonl'):
+        if not os.path.exists(f'main/doccano/{f}.jsonl'):
+            print(f'main/doccano/{f}.jsonl does not exist')
+            continue
+        to_qrels(os.path.basename(f.split('.')[0]))
+        print(LOOKUP)
